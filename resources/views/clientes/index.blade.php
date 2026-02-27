0fa998fe427a2b8fa117facb1e7d11159e673f93
MURILO ALMEIDA DE OLIVEIRA <murilo.oliveira64@senaisp.edu.br>
2026-02-27 16:50:13 -0300

    Aula 27/02

---
+++ resources/views/clientes/index.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header"> <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Confecção') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                @foreach($clientes as $cliente)
+                    <div class="border-b py-4">
+                        <p class="font-bold">{{ $cliente->nome }}</p>
+                        <p class="text-gray-600">{{ $cliente->cpf }}</p>
+                        <p class="text-sm italic">{{ $cliente->telefone }}</p>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</x-app-layout>
